4bab1060c349d9eab076839844ecf43fc968bef7
ferhanayding <ferhanaydin099@gmail.com>
2025-10-20 19:46:55 +0400

    update next.config.ts to include reactStrictMode and output settings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 const createNextIntlPlugin = require('next-intl/plugin');
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', 
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['**/node_modules/**'],
+    },
+  },
+};
+
 module.exports = withNextIntl(nextConfig);
